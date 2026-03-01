27b3336589c26d4b39e0a78c3420e6314dca7385
reysian <71834018+Reysian@users.noreply.github.com>
2026-03-01 13:46:06 -0600

    Prepare app to run on Render.com (first test)

--- server/app.js
+++ server/app.js
@@ -2,21 +2,20 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.get("/api", (req, res) => {
     res.status(200).json({message: "Hello from server!"});
 });
 
 // Deployment
-// Serve static files from client build (AI)
 app.use(express.static(path.join(__dirname, "../client/dist")));
-// Catch all route for react router (AI)
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+app.get('/*splat', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 // Start App
