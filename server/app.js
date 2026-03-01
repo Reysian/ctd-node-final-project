a8e528ab33d6029a19001eb6cafb21fd8a74c954
reysian <71834018+Reysian@users.noreply.github.com>
2026-03-01 16:01:40 -0600

    Install helmet and xss-clean and call inside of app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Extra security
+const cors = require('cors');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+
 app.use(cors());
+app.use(xss());
+app.use(helmet());
 app.use(express.json());
 
 // API routes
 app.get("/api", (req, res) => {
     res.status(200).json({message: "Hello from server!"});
 });
+
+// Not Found
+app.use('/api', (req, res) => {
+    res.status(404).json({error: "Data not found"});
+})
 
 // Deployment
 app.use(express.static(path.join(__dirname, "../client/dist")));
